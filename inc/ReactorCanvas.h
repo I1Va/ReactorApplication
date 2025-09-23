c4abafebcd81703269f137f90a59256bc100492a
Abrutin Ivan Dmitrievich <mai.abrutinid@gmail.com>
2025-09-23 09:40:32 +0300

    Add renderer guard

--- inc/ReactorCanvas.h
+++ inc/ReactorCanvas.h
@@ -1,67 +1,92 @@
-#ifndef REACTORCANVAS_H
-#define REACTORCANVAS_H
+// #ifndef REACTORCANVAS_H
+// #define REACTORCANVAS_H
 
-#include "MyGUI.h"
-#include "ReactorModel.h"
-#include "SDL2/SDL2_gfxPrimitives.h"
+// #include "MyGUI.h"
+// #include "ReactorModel.h"
+// #include "SDL2/SDL2_gfxPrimitives.h"
+
+// Uint32 SDLColorToUint32(SDL_Color c) {
+//     return ((Uint32)c.r << 24) | 
+//            ((Uint32)c.g << 16) | 
+//            ((Uint32)c.b << 8)  | 
+//            ((Uint32)c.a);
+// }
 
 // class MGShape {
-//     SDL_Point position_;
-//     SDL_Color color_;
-
 // public:
 //     MGShape(const SDL_Point &position, const SDL_Color &color): position_(position), color_(color) {};
-//     ~MGShape() = default;
+//     virtual ~MGShape() = default;
 
-//     virtual void draw(SDL_Renderer* renderer) const {
-//         assert(renderer);
+//     void draw() {
+
+//     }
+//     virtual void draw_(SDL_Renderer* renderer) const = 0;
+
+// protected:
+//     SDL_Point position_ = {};
+//     SDL_Color color_ = {};
+// };
+
+// class MGCircle : public MGShape {
+//     int radius_;
+
+// public:
+//     MGCircle(const SDL_Point &position, const int radius, const SDL_Color &color):
+//         MGShape(position, color), radius_(radius) {};
+
+//     void draw(SDL_Renderer* renderer) const override {
+//         filledCircleColor(renderer, (Sint16) position_.x, (Sint16) position_.y, (Sint16) radius_, SDLColorToUint32(color_));
 //     }
 // };
 
-// class MGCircle : MGShape {
-//     double radius_;
+// class MGSquare : public MGShape {
+//     int length_;
 
 // public:
-//     MGCircle(const SDL_Point &position, const double radius, const SDL_Color &color):
-//         MGShape(position, color), radius_(radius) {};
+//     MGSquare(const SDL_Point &position, const int length, const SDL_Color &color):
+//         MGShape(position, color), length_(length) {};
+
+//     void draw(SDL_Renderer* renderer) const override {
+//         SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+//         SDL_RenderFillRect(renderer, &square);
+//     }
+// };
+
+// class ReactorCanvas : public MGWidget {
+//     ReactorModel reactor_ = {};
+
+//     bool nedsRedraw_ = false;
+    
+//     std::vector<MGShape *> geomPrimitives;
+
+// public:
+//     void redrawRector() {
+//         // change current canvas state
+//     }
+
+//     ReactorCanvas
+//     (
+//         const int width, const int height,
+//         std::optional<unsigned int> seed=std::nullopt,
+//         const MGWindow *parent=nullptr
+//     ) : 
+//         MGWidget(width, height, parent),
+//         reactor_(double(width), double(height), [this]() {signalManager_->emit("reactor_updated");}, seed)
+//     {
+//         signalManager_->connect("reactor_updated", [this]() {
+//             this->nedsRedraw_ = true;
+//         });
+//     }
+// private:
+//     void paintEvent(SDL_Renderer* renderer) override {
+//         SDL_Rect widgetRect = {0, 0, width_, height_};
+//         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // MGCanvas BACKGROUND COLOR
+//         SDL_RenderFillRect(renderer, &widgetRect);
+
+//         SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+//         SDL_RenderDrawLine(renderer, 0, 0, 1000, 1000);
+//     }
 // }
 
 
-class ReactorCanvas : public MGWidget {
-    ReactorModel reactor_ = {};
-
-    bool nedsRedraw_ = false;
-    
-    // gm_vector<Shape *> geomPrimitives;
-
-public:
-    void redrawRector() {
-        // change current canvas state
-    }
-
-    ReactorCanvas
-    (
-        const int width, const int height,
-        std::optional<unsigned int> seed=std::nullopt,
-        const MGWindow *parent=nullptr
-    ) : 
-        MGWidget(width, height, parent),
-        reactor_(double(width), double(height), [this]() {signalManager_->emit("reactor_updated");}, seed)
-    {
-        signalManager_->connect("reactor_updated", [this]() {
-            this->nedsRedraw_ = true;
-        });
-    }
-private:
-    void paintEvent(SDL_Renderer* renderer) override {
-        SDL_Rect widgetRect = {0, 0, width_, height_};
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // MGCanvas BACKGROUND COLOR
-        SDL_RenderFillRect(renderer, &widgetRect);
-
-        SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
-        SDL_RenderDrawLine(renderer, 0, 0, 1000, 1000);
-    }
-}
-
-
-#endif // REACTORCANV
+// #endif // REACTORCANV
--- main.cpp
+++ main.cpp
@@ -5,48 +5,44 @@
 #include "ReactorCanvas.h"
 
 
-// const int MAIN_WINDOW_WIDTH = 800;
-// const int MAIN_WINDOW_HEIGHT = 600;
-// const char MAIN_WINDOW_TITLE[] = "MyGUI";
+const int MAIN_WINDOW_WIDTH = 800;
+const int MAIN_WINDOW_HEIGHT = 600;
+const char MAIN_WINDOW_TITLE[] = "MyGUI";
 
-// const char PRESSED_BUTTON_IMAGE_PATH[] = "images/pressedButton.png";
-// const char UNPRESSED_BUTTON_IMAGE_PATH[] = "images/unpressedButton.png";
+const char PRESSED_BUTTON_IMAGE_PATH[] = "images/pressedButton.png";
+const char UNPRESSED_BUTTON_IMAGE_PATH[] = "images/unpressedButton.png";
 
-// void ReactorUpdate() {
-//     std::cout << "Isolate Reactor has updated!\n";
-// }
+void ReactorUpdate() {
+    std::cout << "Isolate Reactor has updated!\n";
+}
 
-// void ReactorAddMolecule() {
-//     std::cout << "ReactorAddMolecule!\n";
-// }
+void ReactorAddMolecule() {
+    std::cout << "ReactorAddMolecule!\n";
+}
 
 int main(void) {
 
-//     MGApplication application;
+    MGApplication application;
     
-//     try {
-//         application.setMainWindow(MAIN_WINDOW_TITLE, MAIN_WINDOW_WIDTH, MAIN_WINDOW_HEIGHT);
-//     } catch (const std::exception& e) {
-//         std::cerr << "FATAL : " << e.what() << "\n";
-//         return -1;
-//     }
+    try {
+        application.setMainWindow(MAIN_WINDOW_TITLE, MAIN_WINDOW_WIDTH, MAIN_WINDOW_HEIGHT);
+    } catch (const std::exception& e) {
+        std::cerr << "FATAL : " << e.what() << "\n";
+        return -1;
+    }
 
-//     // ReactorModel reactor(100, 100, );
+    // ReactorModel reactor(100, 100, );
 
-//     SignalManager *signalManager = application.getSignalManager();
-//     MGMainWindow *mainWindow = application.getMainWindow();
-//     MGWindow *window = mainWindow->addWindow(200, 200, 400, 400);
-//     MGCanvas *canvas = window->addCanvas(30, 30, 1000, 1000);
+    SignalManager *signalManager = application.getSignalManager();
+    MGMainWindow *mainWindow = application.getMainWindow();
+    MGWindow *window = mainWindow->addWindow(100, 100, 400, 400);
     
-    
-    
-    
-//     window->addCanvas(60, 60, 300, 300);
-//     MGButton *button = window->addButton(30, 30, 100, 100, PRESSED_BUTTON_IMAGE_PATH, UNPRESSED_BUTTON_IMAGE_PATH, ReactorAddMolecule);
+    MGButton *button = new MGButton(200, 200, PRESSED_BUTTON_IMAGE_PATH, UNPRESSED_BUTTON_IMAGE_PATH, nullptr, window);
+    window->addWidget(button, {100, 100});
 
-//     application.addEventToMainLoop(ReactorUpdate);
+    // application.addEventToMainLoop(ReactorUpdate);
 
-//     application.run();
-//     return 0;
+    application.run();
+    return 0;
 }
 
