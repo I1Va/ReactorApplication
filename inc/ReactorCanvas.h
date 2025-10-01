2ff0e22b741e85bda6c7cf7db5608d270f4c1809
Abrutin Ivan Dmitrievich <mai.abrutinid@gmail.com>
2025-10-02 01:07:22 +0300

    init

--- inc/ReactorCanvas.h
+++ inc/ReactorCanvas.h
@@ -1,174 +1,174 @@
-#ifndef REACTORCANVAS_H
-#define REACTORCANVAS_H
+// #ifndef REACTORCANVAS_H
+// #define REACTORCANVAS_H
 
-#include "MyGUI.h"
-#include "ReactorModel.h"
-#include "SDL2/SDL2_gfxPrimitives.h"
+// #include "MyGUI.h"
+// #include "ReactorModel.h"
+// #include "SDL2/SDL2_gfxPrimitives.h"
 
-const SDL_Color CIRCLIT_COLOR = {255, 0, 0, 255};
-const SDL_Color QUADRIT_COLOR = {0, 0, 255, 255};
-const double NARROWING_DELTA = 10;
+// const SDL_Color CIRCLIT_COLOR = {255, 0, 0, 255};
+// const SDL_Color QUADRIT_COLOR = {0, 0, 255, 255};
+// const double NARROWING_DELTA = 10;
 
 
-Uint32 SDLColorToUint32(SDL_Color c) {
-    return ((Uint32)c.r << 24) | 
-           ((Uint32)c.g << 16) | 
-           ((Uint32)c.b << 8)  | 
-           ((Uint32)c.a);
-}
+// Uint32 SDLColorToUint32(SDL_Color c) {
+//     return ((Uint32)c.r << 24) | 
+//            ((Uint32)c.g << 16) | 
+//            ((Uint32)c.b << 8)  | 
+//            ((Uint32)c.a);
+// }
 
-class MGShape {
-public:
-    MGShape(const SDL_Point &position, const SDL_Color &color): position_(position), color_(color) {};
-    virtual ~MGShape() = default;
+// class MGShape {
+// public:
+//     MGShape(const SDL_Point &position, const SDL_Color &color): position_(position), color_(color) {};
+//     virtual ~MGShape() = default;
 
-    virtual void draw(SDL_Renderer* renderer) const = 0;
+//     virtual void draw(SDL_Renderer* renderer) const = 0;
 
-protected:
-    SDL_Point position_ = {};
-    SDL_Color color_ = {};
-};
+// protected:
+//     SDL_Point position_ = {};
+//     SDL_Color color_ = {};
+// };
 
-class MGCircle : public MGShape {
-    int radius_;
+// class MGCircle : public MGShape {
+//     int radius_;
 
-public:
-    MGCircle(const SDL_Point &position, const int radius, const SDL_Color &color):
-        MGShape(position, color), radius_(radius) {};
+// public:
+//     MGCircle(const SDL_Point &position, const int radius, const SDL_Color &color):
+//         MGShape(position, color), radius_(radius) {};
 
-    void draw(SDL_Renderer* renderer) const override {
-        filledCircleColor(renderer, (Sint16) position_.x, (Sint16) position_.y, (Sint16) radius_, SDLColorToUint32(color_));
-    }
-};
+//     void draw(SDL_Renderer* renderer) const override {
+//         filledCircleColor(renderer, (Sint16) position_.x, (Sint16) position_.y, (Sint16) radius_, SDLColorToUint32(color_));
+//     }
+// };
 
-class MGSquare : public MGShape {
-    int length_;
+// class MGSquare : public MGShape {
+//     int length_;
 
-public:
-    MGSquare(const SDL_Point &position, const int length, const SDL_Color &color):
-        MGShape(position, color), length_(length) {};
+// public:
+//     MGSquare(const SDL_Point &position, const int length, const SDL_Color &color):
+//         MGShape(position, color), length_(length) {};
 
-    void draw(SDL_Renderer* renderer) const override {
-        RendererGuard rendererGuard(renderer);
+//     void draw(SDL_Renderer* renderer) const override {
+//         RendererGuard rendererGuard(renderer);
 
-        SDL_Rect square = {position_.x - length_ / 2, position_.y - length_ / 2, length_, length_};
-        SDL_SetRenderDrawColor(renderer, color_.r, color_.g, color_.b, color_.a);
-        SDL_RenderFillRect(renderer, &square);
-    }
-};
+//         SDL_Rect square = {position_.x - length_ / 2, position_.y - length_ / 2, length_, length_};
+//         SDL_SetRenderDrawColor(renderer, color_.r, color_.g, color_.b, color_.a);
+//         SDL_RenderFillRect(renderer, &square);
+//     }
+// };
 
-class ReactorCanvas : public MGWidget {
-    ReactorModel reactor_ = {};
+// class ReactorCanvas : public MGWidget {
+//     ReactorModel reactor_ = {};
 
-    bool nedsRedraw_ = false;
+//     bool nedsRedraw_ = false;
     
-    std::vector<MGShape *> geomPrimitives_ = {};
+//     std::vector<MGShape *> geomPrimitives_ = {};
 
-public:
-    ReactorCanvas
-    (
-        const int width, const int height,
-        std::optional<unsigned int> seed=std::nullopt,
-        const MGWindow *parent=nullptr
-    ) : 
-        MGWidget(width, height, parent),
-        reactor_(double(width), double(height), nullptr, seed)
-    {}
+// public:
+//     ReactorCanvas
+//     (
+//         const int width, const int height,
+//         std::optional<unsigned int> seed=std::nullopt,
+//         const MGWindow *parent=nullptr
+//     ) : 
+//         MGWidget(width, height, parent),
+//         reactor_(double(width), double(height), nullptr, seed)
+//     {}
 
-    void update() override {
-        if (!nedsRedraw_) return;
-        nedsRedraw_ = false;
+//     void update() override {
+//         if (!nedsRedraw_) return;
+//         nedsRedraw_ = false;
 
-        for (auto primitive : geomPrimitives_) delete primitive;
-        geomPrimitives_.clear();
+//         for (auto primitive : geomPrimitives_) delete primitive;
+//         geomPrimitives_.clear();
 
-        for (auto molecule : reactor_.getMolecules()) {
-            MGShape *curPrimitive = nullptr;
+//         for (auto molecule : reactor_.getMolecules()) {
+//             MGShape *curPrimitive = nullptr;
         
-            switch (molecule->getType()) {
-                case MoleculeTypes::CIRCLIT:
-                    curPrimitive = (MGShape *) new MGCircle(
-                        {(int) molecule->getPosition().get_x(), (int) molecule->getPosition().get_y()},
-                        molecule->getSize(), CIRCLIT_COLOR);
+//             switch (molecule->getType()) {
+//                 case MoleculeTypes::CIRCLIT:
+//                     curPrimitive = (MGShape *) new MGCircle(
+//                         {(int) molecule->getPosition().get_x(), (int) molecule->getPosition().get_y()},
+//                         molecule->getSize(), CIRCLIT_COLOR);
     
-                    geomPrimitives_.push_back(curPrimitive);
-                    break;
-                case MoleculeTypes::QUADRIT:
-                    curPrimitive = (MGShape *) new MGSquare(
-                        {(int) molecule->getPosition().get_x(), (int) molecule->getPosition().get_y()}, 
-                        molecule->getSize(), QUADRIT_COLOR);
+//                     geomPrimitives_.push_back(curPrimitive);
+//                     break;
+//                 case MoleculeTypes::QUADRIT:
+//                     curPrimitive = (MGShape *) new MGSquare(
+//                         {(int) molecule->getPosition().get_x(), (int) molecule->getPosition().get_y()}, 
+//                         molecule->getSize(), QUADRIT_COLOR);
         
-                    geomPrimitives_.push_back(curPrimitive);
-                    break;
-                default:
-                    assert(0 && "ReactorCanvas update() : unknown moleculeType");
-                    break;
-            }
-        }
-    }
+//                     geomPrimitives_.push_back(curPrimitive);
+//                     break;
+//                 default:
+//                     assert(0 && "ReactorCanvas update() : unknown moleculeType");
+//                     break;
+//             }
+//         }
+//     }
 
-    void addCirclit() {
-        reactor_.addCirclit();
-    }
-    void addQuadrit() {
-        reactor_.addQuadrit();
-    }
-    void removeMolecule() {
-        reactor_.removeMolecule();
-    }
-    void narrowRightWall() {
-        reactor_.narrowRightWall(NARROWING_DELTA);
-    }
-    void unNarrowRightWall() {
-        reactor_.narrowRightWall(-NARROWING_DELTA);
-    }
+//     void addCirclit() {
+//         reactor_.addCirclit();
+//     }
+//     void addQuadrit() {
+//         reactor_.addQuadrit();
+//     }
+//     void removeMolecule() {
+//         reactor_.removeMolecule();
+//     }
+//     void narrowRightWall() {
+//         reactor_.narrowRightWall(NARROWING_DELTA);
+//     }
+//     void unNarrowRightWall() {
+//         reactor_.narrowRightWall(-NARROWING_DELTA);
+//     }
 
-    void showInfo() {
-        double summaryEnergy = 0;
-        for (auto molecule : reactor_.getMolecules()) {
-            summaryEnergy += molecule->getKinecticEnergy() + molecule->getPotentialEnergy();
-        }
-        for (size_t i = 0; i < 4; i++) {
-            std::cout << "wall[" << i << "].energy = " << reactor_.getReactorWalls()[i].energy << ", ";
-        }
+//     void showInfo() {
+//         double summaryEnergy = 0;
+//         for (auto molecule : reactor_.getMolecules()) {
+//             summaryEnergy += molecule->getKinecticEnergy() + molecule->getPotentialEnergy();
+//         }
+//         for (size_t i = 0; i < 4; i++) {
+//             std::cout << "wall[" << i << "].energy = " << reactor_.getReactorWalls()[i].energy << ", ";
+//         }
 
-        std::cout << "systemEnergy : " << summaryEnergy << "\n";
-        for (size_t i = 0; i < 4; i++) {
-            std::cout << "wall[" << i << "].energy = " << reactor_.getReactorWalls()[i].energy << ", ";
-        }
-        std::cout << "\n\n";
-    }
+//         std::cout << "systemEnergy : " << summaryEnergy << "\n";
+//         for (size_t i = 0; i < 4; i++) {
+//             std::cout << "wall[" << i << "].energy = " << reactor_.getReactorWalls()[i].energy << ", ";
+//         }
+//         std::cout << "\n\n";
+//     }
     
-    void reactorUpdate(int frameDelay) {
-        reactor_.update(frameDelay / SEC_TO_MS);
-        width_ = (int) reactor_.getWidth();
-        height_ = (int) reactor_.getHeight();
-        showInfo();
-    }
+//     void reactorUpdate(int frameDelay) {
+//         reactor_.update(frameDelay / SEC_TO_MS);
+//         width_ = (int) reactor_.getWidth();
+//         height_ = (int) reactor_.getHeight();
+//         showInfo();
+//     }
 
-private:
-    void setSignalManager(SignalManager *const signalManager) override {
-        signalManager_ = signalManager;
+// private:
+//     void setSignalManager(SignalManager *const signalManager) override {
+//         signalManager_ = signalManager;
 
-        signalManager_->connect("reactor_updated", [this]() {
-            this->nedsRedraw_ = true;
-        });
+//         signalManager_->connect("reactor_updated", [this]() {
+//             this->nedsRedraw_ = true;
+//         });
 
-        reactor_.setOnUpdate([this]() {signalManager_->emit("reactor_updated");});
-    }
+//         reactor_.setOnUpdate([this]() {signalManager_->emit("reactor_updated");});
+//     }
 
-    void paintEvent(SDL_Renderer* renderer) override {
-        assert(renderer);
+//     void paintEvent(SDL_Renderer* renderer) override {
+//         assert(renderer);
 
-        SDL_Rect widgetRect = {0, 0, width_, height_};
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // MGCanvas BACKGROUND COLOR
-        SDL_RenderFillRect(renderer, &widgetRect);
+//         SDL_Rect widgetRect = {0, 0, width_, height_};
+//         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // MGCanvas BACKGROUND COLOR
+//         SDL_RenderFillRect(renderer, &widgetRect);
 
-        for (auto shape : geomPrimitives_) {
-            shape->draw(renderer);
-        }
-    }
-};
+//         for (auto shape : geomPrimitives_) {
+//             shape->draw(renderer);
+//         }
+//     }
+// };
 
 
-#endif // REACTORCANVAS_H
+// #endif // REACTORCANVAS_H
--- main.cpp
+++ main.cpp
@@ -5,77 +5,89 @@
 #include "ReactorCanvas.h"
 
 
-const int MAIN_WINDOW_WIDTH = 800;
-const int MAIN_WINDOW_HEIGHT = 600;
-const char MAIN_WINDOW_TITLE[] = "MyGUI";
+// const int MAIN_WINDOW_WIDTH = 800;
+// const int MAIN_WINDOW_HEIGHT = 600;
+// const char MAIN_WINDOW_TITLE[] = "MyGUI";
 
-const char NARROW_RIGHTWALL[] = "images/NarrowRightWall.png";
-const char NARROW_RIGHTWALL_PRESSED[] = "images/NarrowRightWallPressed.png";
-const char UNNARROW_RIGHTWALL[] = "images/UnNarrowRightWall.png";
-const char UNNARROW_RIGHTWALL_PRESSED[] = "images/UnNarrowRightWalPressed.png";
+// const char NARROW_RIGHTWALL[] = "images/NarrowRightWall.png";
+// const char NARROW_RIGHTWALL_PRESSED[] = "images/NarrowRightWallPressed.png";
+// const char UNNARROW_RIGHTWALL[] = "images/UnNarrowRightWall.png";
+// const char UNNARROW_RIGHTWALL_PRESSED[] = "images/UnNarrowRightWalPressed.png";
 
-const char ADD_CIRCLIT_PRESSED[] = "images/addCirclitPressed.png";
-const char ADD_CIRCLIT[] = "images/addCirclitUnPressed.png";
-const char ADD_QUADRIT_PRESSED[] = "images/addQuadritPressed.png";
-const char ADD_QUADRIT[] = "images/addQuadritUnPressed.png";
-const char REMOVE_MOLECULE[] = "images/removeMolecule.png";
-const char REMOVE_MOLECULE_PRESSED[] = "images/removeMoleculePressed.png";
+// const char ADD_CIRCLIT_PRESSED[] = "images/addCirclitPressed.png";
+// const char ADD_CIRCLIT[] = "images/addCirclitUnPressed.png";
+// const char ADD_QUADRIT_PRESSED[] = "images/addQuadritPressed.png";
+// const char ADD_QUADRIT[] = "images/addQuadritUnPressed.png";
+// const char REMOVE_MOLECULE[] = "images/removeMolecule.png";
+// const char REMOVE_MOLECULE_PRESSED[] = "images/removeMoleculePressed.png";
 
-const size_t BUTTON_SZ = 50;
-const size_t REACTOR_SZ = 300;
-const size_t REACTOR_BORDER = 30;
+// const size_t BUTTON_SZ = 50;
+// const size_t REACTOR_SZ = 300;
+// const size_t REACTOR_BORDER = 30;
 
-int main(void) {
+// int main(void) {
 
-    MGApplication application;
+//     MGApplication application;
     
-    try {
-        application.setMainWindow(MAIN_WINDOW_TITLE, MAIN_WINDOW_WIDTH, MAIN_WINDOW_HEIGHT);
-    } catch (const std::exception& e) {
-        std::cerr << "FATAL : " << e.what() << "\n";
-        return -1;
-    }
+//     try {
+//         application.setMainWindow(MAIN_WINDOW_TITLE, MAIN_WINDOW_WIDTH, MAIN_WINDOW_HEIGHT);
+//     } catch (const std::exception& e) {
+//         std::cerr << "FATAL : " << e.what() << "\n";
+//         return -1;
+//     }
 
-    SignalManager *signalManager = application.getSignalManager();
-    MGMainWindow *mainWindow = application.getMainWindow();
+//     SignalManager *signalManager = application.getSignalManager();
+//     MGMainWindow *mainWindow = application.getMainWindow();
     
-    MGWindow *reactorWindow = mainWindow->addWindow(0, 0, 800, 600);
+//     MGWindow *reactorWindow = mainWindow->addWindow(0, 0, 800, 600);
     
-    // MGWindow *window = mainWindow->addWindow(200, 200, 400, 400);   
+//     // MGWindow *window = mainWindow->addWindow(200, 200, 400, 400);   
 
 
-    ReactorCanvas *reactorCanvas = new ReactorCanvas(REACTOR_SZ, REACTOR_SZ, std::nullopt, reactorWindow);
-    for (size_t i = 0; i < 400; i++) {
-        reactorCanvas->addCirclit();
-    }
+//     ReactorCanvas *reactorCanvas = new ReactorCanvas(REACTOR_SZ, REACTOR_SZ, std::nullopt, reactorWindow);
+//     for (size_t i = 0; i < 400; i++) {
+//         reactorCanvas->addCirclit();
+//     }
 
-    reactorWindow->addWidget(reactorCanvas, {REACTOR_BORDER, REACTOR_BORDER});
+//     reactorWindow->addWidget(reactorCanvas, {REACTOR_BORDER, REACTOR_BORDER});
 
-    MGButton *addCirclitBtn = new MGButton(BUTTON_SZ, BUTTON_SZ, ADD_CIRCLIT, ADD_CIRCLIT_PRESSED,
-                                           [reactorCanvas](){ reactorCanvas->addCirclit(); }, reactorWindow);
+//     MGButton *addCirclitBtn = new MGButton(BUTTON_SZ, BUTTON_SZ, ADD_CIRCLIT, ADD_CIRCLIT_PRESSED,
+//                                            [reactorCanvas](){ reactorCanvas->addCirclit(); }, reactorWindow);
                                         
-    MGButton *addQuadrittBtn = new MGButton(BUTTON_SZ, BUTTON_SZ, ADD_QUADRIT, ADD_QUADRIT_PRESSED, 
-                                            [reactorCanvas](){ reactorCanvas->addQuadrit(); }, reactorWindow);
+//     MGButton *addQuadrittBtn = new MGButton(BUTTON_SZ, BUTTON_SZ, ADD_QUADRIT, ADD_QUADRIT_PRESSED, 
+//                                             [reactorCanvas](){ reactorCanvas->addQuadrit(); }, reactorWindow);
                                         
-    MGButton *removeMoleculeBtn = new MGButton(BUTTON_SZ, BUTTON_SZ, REMOVE_MOLECULE, REMOVE_MOLECULE_PRESSED, 
-                                               [reactorCanvas](){ reactorCanvas->removeMolecule(); }, reactorWindow);
+//     MGButton *removeMoleculeBtn = new MGButton(BUTTON_SZ, BUTTON_SZ, REMOVE_MOLECULE, REMOVE_MOLECULE_PRESSED, 
+//                                                [reactorCanvas](){ reactorCanvas->removeMolecule(); }, reactorWindow);
                                             
-    MGButton *narrowRightWallBtn = new MGButton(BUTTON_SZ, BUTTON_SZ, NARROW_RIGHTWALL, NARROW_RIGHTWALL_PRESSED, 
-                                                [reactorCanvas](){ reactorCanvas->narrowRightWall(); }, reactorWindow);
+//     MGButton *narrowRightWallBtn = new MGButton(BUTTON_SZ, BUTTON_SZ, NARROW_RIGHTWALL, NARROW_RIGHTWALL_PRESSED, 
+//                                                 [reactorCanvas](){ reactorCanvas->narrowRightWall(); }, reactorWindow);
 
-    MGButton *unNarrowRightWallBtn = new MGButton(BUTTON_SZ, BUTTON_SZ, UNNARROW_RIGHTWALL, UNNARROW_RIGHTWALL_PRESSED, 
-                                                [reactorCanvas](){ reactorCanvas->unNarrowRightWall(); }, reactorWindow);
+//     MGButton *unNarrowRightWallBtn = new MGButton(BUTTON_SZ, BUTTON_SZ, UNNARROW_RIGHTWALL, UNNARROW_RIGHTWALL_PRESSED, 
+//                                                 [reactorCanvas](){ reactorCanvas->unNarrowRightWall(); }, reactorWindow);
 
 
-    reactorWindow->addWidget(addCirclitBtn, {REACTOR_SZ + REACTOR_BORDER, REACTOR_BORDER});
-    reactorWindow->addWidget(addQuadrittBtn, {REACTOR_SZ + REACTOR_BORDER + BUTTON_SZ, REACTOR_BORDER});
-    reactorWindow->addWidget(removeMoleculeBtn, {REACTOR_SZ + REACTOR_BORDER + BUTTON_SZ * 2, REACTOR_BORDER});
-    reactorWindow->addWidget(narrowRightWallBtn, {REACTOR_SZ + REACTOR_BORDER, REACTOR_BORDER + BUTTON_SZ});
-    reactorWindow->addWidget(unNarrowRightWallBtn, {REACTOR_SZ + REACTOR_BORDER + BUTTON_SZ, REACTOR_BORDER + BUTTON_SZ});
+//     reactorWindow->addWidget(addCirclitBtn, {REACTOR_SZ + REACTOR_BORDER, REACTOR_BORDER});
+//     reactorWindow->addWidget(addQuadrittBtn, {REACTOR_SZ + REACTOR_BORDER + BUTTON_SZ, REACTOR_BORDER});
+//     reactorWindow->addWidget(removeMoleculeBtn, {REACTOR_SZ + REACTOR_BORDER + BUTTON_SZ * 2, REACTOR_BORDER});
+//     reactorWindow->addWidget(narrowRightWallBtn, {REACTOR_SZ + REACTOR_BORDER, REACTOR_BORDER + BUTTON_SZ});
+//     reactorWindow->addWidget(unNarrowRightWallBtn, {REACTOR_SZ + REACTOR_BORDER + BUTTON_SZ, REACTOR_BORDER + BUTTON_SZ});
     
-    application.addEventToMainLoop([reactorCanvas](int frameDelay){ reactorCanvas->reactorUpdate(frameDelay); });
+//     application.addEventToMainLoop([reactorCanvas](int frameDelay){ reactorCanvas->reactorUpdate(frameDelay); });
 
-    application.run();
+//     application.run();
+//     return 0;
+// }
+
+int main() {
+    UIManager application();
+
+    Widget *wgt = new Widget(1, 1, 10, 10);
+
+    application.setMainWidget()
+
+
+
+
     return 0;
 }
-
